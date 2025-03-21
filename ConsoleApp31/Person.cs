f8516c6666b768e2d49edf433985b4e1dceb33db
ClyZeR1 <sergeistarodyb@gmail.com>
2025-03-21 11:23:08 +0200

    Add files via upload

---
+++ ConsoleApp31/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    abstract class Person
+    {
+        public string Name { get; set; }
+        public Person(string name)
+        {
+            Name = name;
+        }
+        public abstract void SayHello();
+    }
+    class Russia : Person
+    {
+        public Russia(string name) : base(name) { }
+        public override void SayHello()
+        {
+            Console.WriteLine($"{Name} говорит: Привет!");
+        }
+    }
+    class Ukrainian : Person
+    {
+        public Ukrainian(string name) : base(name) { }
+        public override void SayHello()
+        {
+            Console.WriteLine($"{Name} говорит: Привит!");
+        }
+    }
+    class English : Person
+    {
+        public English(string name) : base(name) { }
+        public override void SayHello()
+        {
+            Console.WriteLine($"{Name} говорит: Hello!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp31/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            people.Add(new Russia("Петя"));
+            people.Add(new Ukrainian("Петро"));
+            people.Add(new English("Pete"));
+
+            foreach (Person person in people)
+            {
+                person.SayHello();
+            }
+        }
+    }
+}
